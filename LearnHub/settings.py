90e2e7199630278ec8d5e732a940454086fcb591
owonikoko olaoluwa <owonikokoolaoluwa@gmail.com>
2020-05-18 06:18:56 +0100

    adding enviropment virables

--- LearnHub/settings.py
+++ LearnHub/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#^g@@v9+$tgpv*m=pf@y#&fsddvdpllu!w-r86-iuxv5n=1zqk'
-
+# SECRET_KEY = '#^g@@v9+$tgpv*m=pf@y#&fsddvdpllu!w-r86-iuxv5n=1zqk'
+SECRET_KEY= os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
