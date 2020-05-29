58aee31783aec8f220c988e8d45b513e162363a6
owonikoko olaoluwa <owonikokoolaoluwa@gmail.com>
2020-05-29 19:54:16 +0100

    repairing learnHub

--- LearnHub/urls.py
+++ LearnHub/urls.py
@@ -19,9 +19,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('user/', include('user.urls')),    
+    path('user/', include('user.urls')), 
+    path('admin/', admin.site.urls),    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
--- home/urls.py
+++ home/urls.py
@@ -5,16 +5,16 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.partner, name="partner"),
-    path('partner', views.partner, name="partner"),
-    path('lecturer', views.lecturer, name="lecturer"),
-    path('upload', views.upload, name="upload"),
-    path('lectures', views.lectures, name="lectures"),
-    path('like', views.like, name='like'),
-    path('view/<int:id>', views.view, name= 'view'),
-    path('<int:id>/details', views.details, name='details')
-    
+    path( '', views.index, name="index" ),
+    path( 'partner', views.partner, name="partner" ),
+    path( 'lecturer', views.lecturer, name="lecturer" ),
+    path( 'upload', views.upload, name="upload" ),
+    path( 'lectures', views.lectures, name="lectures" ),
+    path( 'like', views.like, name='like' ),
+    path( 'view/<int:id>', views.view, name='view' ),
+    path( '<int:id>/details', views.details, name='details' )
+
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static( settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
